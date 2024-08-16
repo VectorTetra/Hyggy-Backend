a3cf798816a2a54fb94cedb04d9a07c3614f9c62
TryastsinEugene <eugenetryas@gmail.com>
2024-08-16 14:16:20 +0300

    Додав співробітників у Bll з сервісами.

---
+++ HyggyBackend.BLL/DTO/EmployeesDTO/EmployeeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HyggyBackend.BLL.DTO.EmployeesDTO
+{
+	public abstract class EmployeeDTO
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Surname { get; set; } = string.Empty;
+		public DateTime DateOfBirth { get; set; }
+	}
+}
