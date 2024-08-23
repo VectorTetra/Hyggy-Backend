116a6f0b9b33a8891d69286b6f268ad1a4ca6169
VectorTetra <viktochonov@gmail.com>
2024-08-23 22:10:26 +0300

    Добавлены сервисы и контроллер для статусов товаров
    В файл `IWareStatusService.cs` добавлен интерфейс `IWareStatusService` для работы со статусами товаров. В файл `WareStatusQueryBLL.cs` добавлен класс `WareStatusQueryBLL` для запросов статусов товаров. В файл `WareStatusService.cs` добавлен класс `WareStatusService`, реализующий интерфейс `IWareStatusService` с использованием AutoMapper и `IUnitOfWork`. В файл `WareStatusController.cs` добавлен контроллер `WareStatusController` для API операций со статусами товаров, использующий `IWareStatusService` и AutoMapper. Внутри контроллера добавлен вспомогательный класс `WareStatusQueryPL` для запросов на уровне представления.

---
+++ HyggyBackend.BLL/Queries/WareStatusQueryBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyggyBackend.BLL.Queries
+{
+    public class WareStatusQueryBLL
+    {
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
+        public long? Id { get; set; }
+        public long? WareId { get; set; }
+        public long? WareArticle { get; set; }
+        public string? NameSubstring { get; set; }
+        public string? DescriptionSubstring { get; set; }
+    }
+}
