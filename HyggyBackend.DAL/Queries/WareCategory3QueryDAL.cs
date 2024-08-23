5335a7a634d040a22cbf45f96404f84f69552624
VectorTetra <viktochonov@gmail.com>
2024-08-23 21:39:03 +0300

    Створено DAL для WareStatus (без Юніту)

--- HyggyBackend.DAL/Queries/WareCategory3QueryDAL.cs
+++ HyggyBackend.DAL/Queries/WareCategory3QueryDAL.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HyggyBackend.DAL.Queries
+﻿namespace HyggyBackend.DAL.Queries
 {
     public class WareCategory3QueryDAL
     {
