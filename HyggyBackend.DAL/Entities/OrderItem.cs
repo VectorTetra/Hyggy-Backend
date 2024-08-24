c1c7a72b7661a541b76d287e72e390f6bd7e4f34
Evgeniy <gzlenko02@gmail.com>
2024-08-24 14:20:12 +0300

    Edit DAL

--- HyggyBackend.DAL/Entities/OrderItem.cs
+++ HyggyBackend.DAL/Entities/OrderItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace HyggyBackend.DAL.Entities
     {
         public long Id { get; set; }
         public long? OrderId { get; set; }
+        [ForeignKey("OrderId")]
         public long? WareId { get; set; }
+        [ForeignKey("WareId")]
         public int OrderCount { get; set; }
         public virtual Order? Order { get; set; }
         public virtual Ware? Ware { get; set; }
